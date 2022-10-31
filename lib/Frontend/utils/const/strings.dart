05ae11f20da717a84aa8e505a4e0cc6e1eb890ea
Atul Tiwari <tiwariatul9526@gmail.com>
2022-10-31 17:19:28 +0530

    project setup

---
+++ lib/Frontend/utils/const/strings.dart
@@ -0,0 +1,2 @@
+const appName = "Apna Store";
+const appVersion = "Version 1.0.0";
---
+++ lib/Frontend/widgets/app_logo.widgets.dart
@@ -0,0 +1,6 @@
+import 'package:ecommerce_store/Frontend/utils/const/const.dart';
+
+Widget appLogoWidget() {
+  //using velocity-x
+  return Image.asset(icAppLogo)
+}
